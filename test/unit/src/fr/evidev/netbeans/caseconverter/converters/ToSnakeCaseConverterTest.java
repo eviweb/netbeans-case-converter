69140ee1b7bf0f60cffd41c9b48cbcfeacc98ce3
Eric Villard <dev@eviweb.fr>
2019-05-23 13:51:44 +0200

    add ToSnakeCaseConverter
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ test/unit/src/fr/evidev/netbeans/caseconverter/converters/ToSnakeCaseConverterTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2019 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.evidev.netbeans.caseconverter.converters;
+
+/**
+ *
+ * @author Eric VILLARD <dev@eviweb.fr>
+ */
+public class ToSnakeCaseConverterTest extends ConverterTestBase {
+
+    @Override
+    protected void registerScenarii() {
+        String expected = "some_dummy_text";
+        addScenarii(
+                new ConversionScenario() {
+            @Override
+            public String getSourceText() {
+                return "Some dummy   Text";
+            }
+
+            @Override
+            public String getExpectedResult() {
+                return expected;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Convert simple text to snake case.";
+            }
+        },
+                new ConversionScenario() {
+            @Override
+            public String getSourceText() {
+                return "some-dummy---text";
+            }
+
+            @Override
+            public String getExpectedResult() {
+                return expected;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Convert hyphen case like text to snake case.";
+            }
+        },
+                new ConversionScenario() {
+            @Override
+            public String getSourceText() {
+                return "Some Dummy Text";
+            }
+
+            @Override
+            public String getExpectedResult() {
+                return expected;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Convert title case like text to snake case.";
+            }
+        },
+                new ConversionScenario() {
+            @Override
+            public String getSourceText() {
+                return "SomeDummyText";
+            }
+
+            @Override
+            public String getExpectedResult() {
+                return expected;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Convert CamelCase like text to snake case.";
+            }
+        });
+    }
+}
