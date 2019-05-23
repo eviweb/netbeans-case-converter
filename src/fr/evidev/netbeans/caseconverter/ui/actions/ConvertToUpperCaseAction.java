594019331cb83cf9a27cba7fbe3866899dde3362
Eric Villard <dev@eviweb.fr>
2019-05-23 14:33:57 +0200

    add ConvertToUpperCaseAction
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ src/fr/evidev/netbeans/caseconverter/ui/actions/ConvertToUpperCaseAction.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.evidev.netbeans.caseconverter.ui.actions;
+
+import fr.evidev.netbeans.caseconverter.converters.ConverterFactory;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "ConvertTo/SubActions",
+        id = "fr.evidev.netbeans.caseconverter.ui.actions.ConvertToUpperCaseAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ConvertToUpperCaseAction"
+)
+@Messages("CTL_ConvertToUpperCaseAction=UPPERCASE")
+public final class ConvertToUpperCaseAction extends ConvertToAction {
+
+    @Override
+    protected ConverterFactory.CONVERTER getConverterType() {
+        return ConverterFactory.CONVERTER.UPPERCASE;
+    }
+}
