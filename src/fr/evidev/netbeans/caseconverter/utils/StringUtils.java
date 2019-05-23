b98991dee349ba54f79b3fe5cad3da9b584234e8
Eric Villard <dev@eviweb.fr>
2019-05-23 11:47:32 +0200

    add StringUtils
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ src/fr/evidev/netbeans/caseconverter/utils/StringUtils.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.evidev.netbeans.caseconverter.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utilities for string operations
+ *
+ * @author Eric VILLARD <dev@eviweb.fr>
+ */
+public final class StringUtils {
+
+    public static String capitalizeFirstChar(String text) {
+        return text.isEmpty() ? text : text.substring(0, 1).toUpperCase().concat(text.substring(1));
+    }
+
+    public static String capitalizePattern(String text, String pattern) {
+        Matcher m = Pattern.compile("(" + pattern + ")").matcher(text);
+        StringBuilder sb = new StringBuilder();
+        int last = 0;
+
+        while (m.find()) {
+            sb.append(text.substring(last, m.start()));
+            sb.append(m.group(1).toUpperCase());
+            last = m.end();
+        }
+        sb.append(text.substring(last));
+
+        return sb.toString();
+    }
+
+    public static String puncuateWithUnique(String text, String punctuationMark) {
+        return text.endsWith(punctuationMark) ? text : text.concat(punctuationMark);
+    }
+
+    public static String replaceDuplicatesWithUnique(String text, String pattern, String replacement) {
+        return text.replaceAll(pattern, replacement);
+    }
+
+    public static String stripPatternFrom(String text, String pattern) {
+        return stripLeadingPatternFrom(stripTrailingPatternFrom(text, pattern), pattern);
+    }
+
+    public static String stripLeadingPatternFrom(String text, String pattern) {
+        return text.replaceFirst("^" + escapeMetaChars(pattern), "");
+    }
+
+    public static String stripTrailingPatternFrom(String text, String pattern) {
+        return text.replaceFirst(escapeMetaChars(pattern) + "$", "");
+    }
+
+    public static String prefixPatternWith(String text, String pattern, String prefix) {
+        return text.replaceAll("(" + pattern + ")", prefix + "$1");
+    }
+
+    public static String escapeMetaChars(String text) {
+        String pattern = "([\\<\\>\\(\\)\\[\\]\\{\\}\\\\\\^\\-\\=\\$\\!\\ \\|\\?\\*\\+\\.])";
+
+        return text.replaceAll(pattern, "\\\\$1");
+    }
+}
---
+++ test/unit/src/fr/evidev/netbeans/caseconverter/utils/StringUtilsTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2019 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.evidev.netbeans.caseconverter.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Eric VILLARD <dev@eviweb.fr>
+ */
+public class StringUtilsTest {
+
+    /**
+     * Test of capitalizeFirstChar method, of class StringUtils.
+     */
+    @Test
+    public void testCapitalizeFirstChar() {
+        String source = "some Dummy text";
+        String expected = "Some Dummy text";
+
+        assertEquals(expected, StringUtils.capitalizeFirstChar(source));
+    }
+
+    @Test
+    public void testCapitalizeFirstCharOnEmptyString() {
+        assertTrue(StringUtils.capitalizeFirstChar("").isEmpty());
+    }
+
+    @Test
+    public void testCapitalizeFirstCharOnSingleCharString() {
+        String source = "s";
+        String expected = "S";
+
+        assertEquals(expected, StringUtils.capitalizeFirstChar(source));
+        assertEquals(expected, StringUtils.capitalizeFirstChar(source.toUpperCase()));
+    }
+
+    /**
+     * Test of capitalizePattern method, of class StringUtils.
+     */
+    @Test
+    public void testCapitalizePattern() {
+        String source = "some Dummy text";
+        String expected = "soMe DuMMy text";
+
+        assertEquals(expected, StringUtils.capitalizePattern(source, "m"));
+    }
+
+    @Test
+    public void testCapitalizePatternOnEmptyString() {
+        assertTrue(StringUtils.capitalizePattern("", "m").isEmpty());
+    }
+
+    /**
+     * Test of puncuateWithUnique method, of class StringUtils.
+     */
+    @Test
+    public void testPuncuateWithUnique() {
+        String dot = ".";
+        String source = "some Dummy text";
+        String expected = source + dot;
+
+        assertEquals(expected, StringUtils.puncuateWithUnique(source, dot));
+    }
+
+    @Test
+    public void testPoncuateWithUniqueOnEmptyString() {
+        String dot = ".";
+
+        assertEquals(dot, StringUtils.puncuateWithUnique("", dot));
+    }
+
+    @Test
+    public void testPoncuateWithUniqueOnAlreadyPunctuatedString() {
+        String etc = "...";
+        String source = "some Dummy text" + etc;
+        String expected = source;
+
+        assertEquals(expected, StringUtils.puncuateWithUnique(source, etc));
+    }
+
+    /**
+     * Test of replaceDuplicatesWithUnique method, of class StringUtils.
+     */
+    @Test
+    public void testReplaceDuplicatesWithUnique() {
+        String source = "Some  Dummy     text";
+        String expected = "Some Dummy text";
+
+        assertEquals(expected, StringUtils.replaceDuplicatesWithUnique(source, "\\s+", " "));
+        assertEquals(expected, StringUtils.replaceDuplicatesWithUnique(source.replaceAll(" ", "-"), "-+", " "));
+    }
+
+    @Test
+    public void testRemoveDuplicatesOnEmptyString() {
+        assertTrue(StringUtils.replaceDuplicatesWithUnique("", "\\s+", " ").isEmpty());
+    }
+
+    /**
+     * Test of stripPatternFrom method, of class StringUtils.
+     */
+    @Test
+    public void testStripPatternFrom() {
+        String pattern = "***";
+        String expected = "Some Dummy text";
+        String source = pattern + expected + pattern;
+
+        assertEquals(expected, StringUtils.stripPatternFrom(source, pattern));
+    }
+
+    @Test
+    public void testStripPatternFromOnEmptyString() {
+        assertTrue(StringUtils.stripPatternFrom("", "***").isEmpty());
+    }
+
+    @Test
+    public void testStripPatternFromStringWithPatternInside() {
+        String pattern = "***";
+        String expected = "Some Dummy " + pattern + " text";
+        String source = expected + pattern;
+
+        assertEquals(expected, StringUtils.stripPatternFrom(source, pattern));
+    }
+
+    /**
+     * Test of stripLeadingPatternFrom method, of class StringUtils.
+     */
+    @Test
+    public void testStripLeadingPatternFrom() {
+        String pattern = "***";
+        String expected = "Some Dummy " + pattern + " text" + pattern;
+        String source = pattern + expected;
+
+        assertEquals(expected, StringUtils.stripLeadingPatternFrom(source, pattern));
+    }
+
+    /**
+     * Test of stripTrailingPatternFrom method, of class StringUtils.
+     */
+    @Test
+    public void testStripTrailingPatternFrom() {
+        String pattern = "***";
+        String expected = pattern + "Some Dummy " + pattern + " text";
+        String source = expected + pattern;
+
+        assertEquals(expected, StringUtils.stripTrailingPatternFrom(source, pattern));
+    }
+
+    /**
+     * Test of prefixPatternWith method, of class StringUtils.
+     */
+    @Test
+    public void testPrefixPatternWith() {
+        String prefix = "***";
+        String source = "someDummyText";
+        String expected = "some" + prefix + "Dummy" + prefix + "Text";
+
+        assertEquals(expected, StringUtils.prefixPatternWith(source, "[A-Z]", prefix));
+    }
+
+    @Test
+    public void testPrefixPatternWithOnEmptyString() {
+        assertTrue(StringUtils.prefixPatternWith("", "[A-Z]", "***").isEmpty());
+    }
+
+    /**
+     * Test of escapeMetaChars method, of class StringUtils.
+     */
+    @Test
+    public void testEscapeMetaChars() {
+        String source = "<>()[]{}\\^-=$! |?*+.";
+        String expected = "\\<\\>\\(\\)\\[\\]\\{\\}\\\\\\^\\-\\=\\$\\!\\ \\|\\?\\*\\+\\.";
+
+        assertEquals(expected, StringUtils.escapeMetaChars(source));
+    }
+
+}
